0f104854a28dc38fc1d1f3f9e9cb9050b0fa7f4a
jose6941 <jose.souza1@aluno.ifsp.edu.br>
2026-04-16 20:21:44 -0300

    Ocultando credenciais expostas na API do projeto

--- front/components/firebaseConfig.js
+++ front/components/firebaseConfig.js
@@ -3,13 +3,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATcCZaQ6AZr5pTm2HdV_1Oa4uBwWO2Uoc",
-  authDomain: "dietaapp-f4103.firebaseapp.com",
-  projectId: "dietaapp-f4103",
-  storageBucket: "dietaapp-f4103.firebasestorage.app",
-  messagingSenderId: "1087802466144",
-  appId: "1:1087802466144:web:625c58b7f51f0591810805",
-  measurementId: "G-FETT5SLDFL"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
